a70435d0dbb5fb3cc93d83f848b33277613d28b7
sean-b765 <61043071+sean-b765@users.noreply.github.com>
2021-08-16 11:03:19 +0800

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div
+			style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		text-align: center;
+		width: 100vw;
+		padding: 5rem;"
+		>
+			💰 Welcome to the start of something new...
+		</div>
+	)
+}
+
+export default App
