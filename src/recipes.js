868b4ab2986237230895ec7ce30e5ea1cf119b9c
sean-b765 <61043071+sean-b765@users.noreply.github.com>
2021-08-16 21:10:24 +0800

    Simulate API with array

---
+++ src/recipes.js
@@ -0,0 +1,28 @@
+export default [
+	{
+		id: 0,
+		title: 'Chicken Teriyaki',
+		image_url:
+			'https://www.lecremedelacrumb.com/wp-content/uploads/2018/03/baked-teriyaki-chicken-102.jpg',
+		author: {
+			name: 'Jane Cook',
+			avatar:
+				'https://www.adobe.com/content/dam/cc/us/en/creativecloud/photography/discover/portrait-photography/CODERED_B1_portrait_photography-P4a_438x447.jpg.img.jpg',
+		},
+		prep_time: 30,
+		serves: 4,
+	},
+	{
+		id: 1,
+		title: 'Pumpkin Pasties',
+		author: {
+			name: 'Bathilda Baker',
+			avatar:
+				'https://www.anthropics.com/portraitpro/img/page-images/homepage/v21/new-features-A.jpg',
+		},
+		image_url:
+			'https://theloopywhisk.com/wp-content/uploads/2018/10/Pumpkin-Flaky-Pastries_730px-featured-500x500.jpg',
+		prep_time: 120,
+		serves: 4,
+	},
+]
