4582b917027d6d00ac3661f7262a9fa78a30be07
sean-b765 <61043071+sean-b765@users.noreply.github.com>
2021-10-09 11:42:42 +0800

    Update

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -18,12 +18,37 @@ const Recipe = ({ id }) => {
 			.catch((err) => console.log(err))
 	}, [])
 
+	const imagePattern = /(![\[][\w\d-_]{0,})[\]]/g
+
+	/**
+	 * Aim of this method is to split body into array via newline character or ![
+	 * @param {string} body
+	 */
+	const getArray = (body) => body.split('\n')
+
+	/**
+	 *
+	 * @param {string} content
+	 */
+	const getElement = (content) => {
+		if (imagePattern.test(content)) {
+			// there is a image
+		} else {
+			// plain text
+		}
+	}
+
 	return (
 		<motion.section className="fullrecipe">
 			{post ? (
 				<div>
 					<header>{post.title}</header>
 					<p>{post.content}</p>
+					<article>
+						{getArray(post.method).map((paragraph) => (
+							<p>{paragraph}</p>
+						))}
+					</article>
 				</div>
 			) : (
 				<Spinner />
