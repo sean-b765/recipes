52a41973923b4f8f96d089c81d3f6ea86e87fe8f
sean-b765 <sean.b0153@gmail.com>
2021-10-13 14:47:46 +0800

    Update button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import { MdDeleteForever, MdEdit } from 'react-icons/md'
 
 const Button = ({ onClick, type }) => {
 	if (!['delete', 'edit'].includes(type))
 		throw new Error(`Invalid type "${type}" in Button.jsx`)
 
 	let cName = 'btn btn--pill '
+	let ariaLabel = ''
 
-	if (type === 'delete') cName += 'btn--red'
-	else if (type === 'edit') cName += 'btn--blue'
+	if (type === 'delete') {
+		cName += 'btn--red'
+		ariaLabel = 'Delete post'
+	} else if (type === 'edit') {
+		cName += 'btn--blue'
+		ariaLabel = 'Edit post'
+	}
 
 	return (
-		<button onClick={onClick} className={cName}>
-			{type === 'delete' && 'Delete Post'}
-			{type === 'edit' && 'Edit Post'}
+		<button
+			onClick={onClick}
+			className={cName}
+			aria-label={ariaLabel}
+			alt={ariaLabel}
+		>
+			{type === 'delete' && <MdDeleteForever />}
+			{type === 'edit' && <MdEdit />}
 		</button>
 	)
 }
