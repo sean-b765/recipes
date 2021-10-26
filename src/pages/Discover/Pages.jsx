26a63cb8b5791a40471d7336fbcd524e614db3fa
sean-b765 <sean.b0153@gmail.com>
2021-10-26 16:13:29 +0800

    Clicking page number navigates to that page

--- src/pages/Discover/Pages.jsx
+++ src/pages/Discover/Pages.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
+import { useHistory } from 'react-router'
 
 const Pages = ({ setFilters, filters }) => {
+	const history = useHistory()
+
 	let page_arr = []
 	for (
-		let i = filters.page - 4 < 0 ? 0 : filters.page - 4;
-		i < filters.page + 4;
+		let i = filters.page - 2 < 1 ? 1 : filters.page - 2;
+		i < filters.page + 3 && i <= filters.pageCount;
 		i++
 	) {
 		page_arr.push(i)
 	}
 	return (
 		<>
-			{page_arr.map((page) =>
+			{page_arr.map((page, idx) =>
 				page === filters.page ? (
 					<span
+						style={{ cursor: 'default' }}
+						key={idx}
 						aria-label={`Showing results for current page`}
 						className="btn btn--no-bg btn--page btn--page-selected"
 					>
@@ -21,8 +26,12 @@ const Pages = ({ setFilters, filters }) => {
 					</span>
 				) : (
 					<button
+						key={idx}
 						aria-label={`Show results for page ${page}`}
 						className="btn btn--no-bg btn--page"
+						onClick={() => {
+							setFilters({ ...filters, page: page })
+						}}
 					>
 						{page}
 					</button>
