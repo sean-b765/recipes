5adaf4c026e3d8b4da7d6ad0425dc246403b98a4
sean-b765 <sean.b0153@gmail.com>
2021-11-16 13:09:12 +0800

    Listen to axios events to show a loading animation

--- src/App.js
+++ src/App.js
@@ -9,13 +9,18 @@ import Create from './pages/Create/Create'
 import Edit from './pages/Create/Edit'
 import { Route, Switch, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import Settings from './pages/Settings/Settings'
+import Spinner from './components/Spinner'
+
+import emitter from './_services/emitter'
 
 function App() {
 	const dispatch = useDispatch()
 	const location = useLocation()
+
+	const [loading, setLoading] = useState(false)
 
 	useEffect(() => {
 		// Initialize the AUTH user state from browser's localstorage
@@ -23,12 +28,30 @@ function App() {
 			type: 'AUTH/INIT',
 			payload: JSON.parse(localStorage.getItem('profile')),
 		})
+
+		// Listen to axios events via our emitter to handle request/response event
+		emitter.on('REQUEST_START', (e) => {
+			setLoading(true)
+		})
+		emitter.on('RESPONSE_FINISH', (e) => {
+			setLoading(false)
+		})
+		emitter.on('RESPONSE_ERROR', (e) => {
+			setLoading(false)
+		})
+
+		return () => {
+			emitter.off('RESPONSE_FINISH')
+			emitter.off('RESPONSE_ERROR')
+			emitter.off('REQUEST_START')
+		}
 	}, [])
 
 	return (
 		<>
 			<Navbar />
 			<main className="container">
+				<Spinner showing={loading} />
 				<AnimatePresence exitBeforeEnter>
 					<Switch location={location} key={location.pathname}>
 						<Route path="/" exact>
